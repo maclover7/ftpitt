6e1a45efae2bdd491bda10f5e82ceda6a88dc7c5
Jon Moss <me@jonathanmoss.me>
2023-07-18 12:40:54 -0400

    Outline bash script

---
+++ import.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Appeals
+
+# Assessments
+
+# Parcel boundaries
+
+# Parcel centroids
+
+# Parcel delinquencies (Pittsburgh)
+
+# Pittsburgh-owned parcels
+
+# URA-owned parcels
+
+# Pittsburgh facilities
+
+# Parcel liens
+
+# Pittsburgh-owned parcel sales
+
+# Parcel violations (Pittsburgh)
